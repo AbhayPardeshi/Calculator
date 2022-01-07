e3394678b6e8232346152518ba7bbecfddd1fdb4
Abhay <abhaypardeshi2210@gmail.com>
2022-01-07 21:00:32 +0530

    Js addition

---
+++ main.js
@@ -0,0 +1,14 @@
+class Calculator{
+constructor(currentOperandTextElement,previousOperandTextElement)
+
+}
+
+
+
+const numberButton =document.querySelectorAll("[data-number]")
+const operationButton =document.querySelectorAll("[data-operation]")
+const deleteButton =document.querySelector("[data-delete]")
+const allClearButton =document.querySelector("[data-all-clear]")
+const equalsButton =document.querySelector("[data-equal]")
+const previousOperandTextElement =document.querySelector("[data-previous-operand]")
+const currentOperandTextElement =document.querySelector("[data-current-operand")
